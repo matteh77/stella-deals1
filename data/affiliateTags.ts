74927d443c45d0d97aa49e021a40268c4c0e4582
matteh77 <matteocarbino@gmail.com>
2026-05-07 19:31:27 +0200

    Update affiliateTags.ts

--- data/affiliateTags.ts
+++ data/affiliateTags.ts
@@ -1,2 +1,9 @@
 import type { RegionCode } from "./locales";
-export const affiliateTags: Record<RegionCode, string> = { it: "", de: "", fr: "", es: "", uk: "" };
+
+export const affiliateTags: Record<RegionCode, string> = {
+  it: "stelladealsit-21",
+  de: "stelladealsit-21",
+  fr: "stelladealsit-21",
+  es: "stelladealsit-21",
+  uk: "stelladealsit-21"
+};
